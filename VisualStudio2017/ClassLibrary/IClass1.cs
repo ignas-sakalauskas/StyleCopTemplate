55da6d74179633c82cc2a9a2f651b3fa76bf3bf8
ignas-sakalauskas <ignas.sakal@outlook.com>
2017-04-23 12:04:19 +0100

    Add solution files, and initial style cop settings.

---
+++ VisualStudio2017/ClassLibrary/IClass1.cs
@@ -0,0 +1,20 @@
+﻿// <copyright file="IClass1.cs" company="Test Company">
+// This source code is Copyright © Test Company and MAY NOT be copied, reproduced,
+// published, distributed or transmitted to or stored in any manner without prior
+// written consent from Test Company (www.yourcompany.com).
+// </copyright>
+
+namespace ClassLibrary
+{
+    /// <summary>
+    /// Simple test class
+    /// </summary>
+    public interface IClass1
+    {
+        /// <summary>
+        /// Test method to return non-empty text string
+        /// </summary>
+        /// <returns>A string contianng a name of this method, and when it was called</returns>
+        string GetTestStats();
+    }
+}
